3a0ed0741b9153f05fd94bf78b2ca39cef53faa1
Clairton Santos <hi@clairton.dev>
2024-08-30 18:22:57 -0300

    feat: Alert Event

---
+++ src/main/java/ac/grim/grimac/api/events/AlertEvent.java
@@ -0,0 +1,46 @@
+package ac.grim.grimac.api.events;
+
+import ac.grim.grimac.api.AbstractCheck;
+import ac.grim.grimac.api.GrimUser;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class AlertEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final GrimUser user;
+    private final AbstractCheck check;
+    private final Map<String, Object> details;
+    private final Collection<Player> receivers;
+
+    private @Setter boolean cancelled;
+
+    public AlertEvent(GrimUser user, AbstractCheck check, Map<String, Object> details, Collection<Player> receivers) {
+        this.user = user;
+        this.check = check;
+        this.details = details;
+        this.receivers = receivers;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
