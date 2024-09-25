e691d1ae36bbfdfce41c8705e32f47b51cdb56d8
Clairton Santos <hi@clairton.dev>
2024-09-24 21:05:36 -0300

    fix: Change Player to GrimUser on Punish event

--- src/main/java/ac/grim/grimac/api/events/PunishEvent.java
+++ src/main/java/ac/grim/grimac/api/events/PunishEvent.java
@@ -1,9 +1,9 @@
 package ac.grim.grimac.api.events;
 
 import ac.grim.grimac.api.AbstractCheck;
+import ac.grim.grimac.api.GrimUser;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.NotNull;
 @Getter @AllArgsConstructor
 public class PunishEvent extends Event {
 
-    private final Player player;
+    private final GrimUser grimUser;
     private final AbstractCheck check;
 
     private static final HandlerList handlers = new HandlerList();
